c5b0d71f9fdba6b55e812cf870f3b771b459722b
makenova <themarchmyth@gmail.com>
2015-10-05 01:41:02 -0500

    {remove site,manage nginx} option working

--- README.md
+++ README.md
@@ -1,17 +1,59 @@
 # nodeginx
 
-This is a work in progress.
+This is a work in progress, built with my narrow use case in mind.
 
-Nodeginx is a CLI helper for managing [Nginx](https://www.nginx.com) configs.
+Nodeginx is a CLI helper for managing [Nginx](https://www.nginx.com) virtual
+host configs. This assumes you are using the sites-available and
+sites-enabled setup, where those directories are arranged as so,
+
+```
+/etc
+|-- nginx
+     |-- nginx.config
+     |-- sites-enabled
+     |    |-- site-one
+     |    +-- site-two
+     +-- sites-available
+          |-- site-two
+```
+
+server blocks are setup in the sites-available directory and are "turned on"
+by creating a symbolic link to the sites-enabled directory. In the example
+above, site-one and site-two are setup but only site-two is served by nginx.
 
 ## Installation
 
-This is a scoped [NPM package](https://www.npmjs.com), [Node.js](https://nodejs.org/en/) is a prerequisite.
+This is a scoped [NPM](https://www.npmjs.com) package, [Node.js](https://nodejs.org/en/) is a prerequisite.
 
 ```
 $ npm install --g @makenova/nodeginx
 ```
 
+## Use
+
+Type `nodeginx` on the command line, your sites should be listed, and you
+will be prompted. The nginx config files are usually in a directory that
+requires root permission. You may be prompted for you password if your user is
+not set up for pasword-less sudo (not tested).
+
+```
+makenova@gia:~$ nodeginx
+✔ is enabled
+✘ is disabled
+
+default ✘
+makenova ✔
+votebot ✔
+webtask ✘
+
+
+? What would you like to do? (Use arrow keys)
+❯ enable/disable a site
+  add a site
+  remove a site
+  start/stop/restart nginx
+  exit
+```
 ## Bugs
 Please report any bugs to: <https://github.com/makenova/nodeginx/issues>
 
--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var async = require('async');
 var inquirer = require('inquirer');
 var exec = require('child_process').exec;
 
+// TODO: account for alternate install locations
+// http://nginx.org/en/docs/beginners_guide.html
+// nginx may be installed in /usr/local/nginx/conf, /etc/nginx, or
+// /usr/local/etc/nginx.
+
 // nginx constants
 const NGINX_PATH = '/etc/nginx/';
 const sitesAvailableStr = 'sites-available';
@@ -14,25 +19,23 @@ const CR = '\n';
 
 // actions
 const toggleSiteStr = 'enable/disable a site';
-const manageSiteStr = 'add/remove a site';
-const restartNginxStr = 'start/stop/restart nginx';
+const addSiteStr = 'add a site';
+const removeSiteStr = 'remove a site';
+const manageNginxStr = 'start/stop/restart nginx';
 const exitProgramStr = 'exit';
 
-// var cmdArgs = process.argv.slice(2);
-
 fs.readdir(NGINX_PATH, (err, files) => {
-  if (err) throw err;
-  // if (err) process.exit(1);
+  bail(err);
   var sitesFolders = files.some((file)=>{
     return file === sitesAvailableStr || file === sitesEnabledStr;
   });
   if (sitesFolders){
     fs.readdir(NGINX_PATH + sitesAvailableStr, (err, files) => {
       'use strict';
-      if (err) throw err;
+      bail(err);
       let sitesAvailable = files;
       fs.readdir(NGINX_PATH + sitesEnabledStr, (err, files) => {
-        if (err) throw err;
+        bail(err);
         let sitesEnabled = files;
 
         // print list of sites, marked those that are enabled
@@ -60,8 +63,9 @@ fs.readdir(NGINX_PATH, (err, files) => {
             message: 'What would you like to do?',
             choices: [
               toggleSiteStr,
-              manageSiteStr,
-              restartNginxStr,
+              addSiteStr,
+              removeSiteStr,
+              manageNginxStr,
               exitProgramStr
             ]
           },
@@ -75,29 +79,65 @@ fs.readdir(NGINX_PATH, (err, files) => {
             }
           },
           {
-            type: 'list',
-            name: 'askManageSite',
-            message: 'Select site to ',
-            choices: [],
+            type: 'input',
+            name: 'askAddSite',
+            message: 'Enter path to config file',
             when: function (answers){
-              return answers.action === manageSiteStr;
+              return answers.action === addSiteStr;
+            }
+          },
+          {
+            type: 'list',
+            name: 'askRemoveSite',
+            message: 'Select a site to remove',
+            choices: sitesAvailable,
+            when: function (answers){
+              return answers.action === removeSiteStr;
+            }
+          },
+          {
+            type: 'confirm',
+            name: 'askConfirmRemoveSite',
+            message: (answers)=>{return `Are you sure you want to remove ${answers.askRemoveSite}:`;},
+            default: false,
+            when: function (answers){
+              return answers.action === removeSiteStr;
+            }
+          },
+          {
+            type: 'list',
+            name: 'askManageNginx',
+            message: `Choose action:`,
+            choices: [ 'start', 'stop', 'restart', 'reload', 'force-reload',
+              'status', 'configtest', 'rotate', 'upgrade' ],
+            default: false,
+            when: function (answers){
+              return answers.action === manageNginxStr;
             }
           }
         ];
 
-        // prompt user for action
+        // prompt user for action and handle user answers
         inquirer.prompt(questions, function (answers){
-          // console.log(answers);
-          if(answers.askToggleSite){
+          if (answers.askToggleSite) {
             toggleSites(sitesEnabled, answers.askToggleSite, (err)=>{
-              if(err){
-                console.log(err);
-                process.exit(1);
-              }
+              bail(err);
             });
+          }else if (answers.askAddSite) {
+            addSite(answers.askAddSite);
+          }else if (answers.askConfirmRemoveSite) {
+            removeSite(answers.askRemoveSite, (err)=>{
+              bail(err);
+              gracefulExit();
+            });
+          }else if (answers.askManageNginx) {
+            manageNginx(answers.askManageNginx, (err)=>{
+              bail(err);
+            });
+          }else {
+            gracefulExit();
           }
         });
-
       });
     });
   }
@@ -109,6 +149,33 @@ function xorleft (array0, array1){
     return !array1.some(array1element=>{
       return array1element === array0element;
     });
+  });
+}
+
+function gracefulExit() {
+  console.log('Bye!');
+  process.exit(0);
+}
+
+function bail(err){
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+function fileExists(filepath){
+  fs.stat(filepath, (err, stats)=>{
+    return Boolean(stats);
+  });
+}
+
+function sudoRemove(filepath, callback) {
+  // Would prefer `fs.unlink` but, I don't know how to make it work with sudo
+  exec(`sudo rm ${filepath}`, (err, stdout, stderr)=>{
+    if(err) return callback(err);
+    console.log(stdout, stderr);
+    callback();
   });
 }
 
@@ -128,21 +195,18 @@ function manageNginx(action, callback) {
 
 // Toggle site function and helpers
 function enableSite (site, callback){
+  // would prefer `fs.symlink` but, sudo
   exec(`sudo ln -s ${NGINX_PATH}${sitesAvailableStr}/${site} ${NGINX_PATH}${sitesEnabledStr}/${site}`, (err, stdout, stderr)=>{
-    if(err){
-      return callback(err);
-    }
+    if(err) return callback(err);
     console.log(`enable ${site}`, stdout, stderr);
     callback();
   });
 }
 
 function disableSite (site, callback){
-  exec(`sudo rm ${NGINX_PATH}${sitesEnabledStr}/${site}`, (err, stdout, stderr)=>{
-    if(err){
-      return callback(err);
-    }
-    console.log(`disable ${site}`, stdout, stderr);
+  sudoRemove(`${NGINX_PATH}${sitesEnabledStr}/${site}`, (err)=>{
+    if(err) return callback(err);
+    console.log(`disabled ${site}`);
     callback();
   });
 }
@@ -150,17 +214,17 @@ function disableSite (site, callback){
 function toggleSites (sitesEnabled, askToggleSiteAnswers, toggleDoneCB){
   async.series([
     // enable sites
-    function(callback){
+    (callback)=>{
       async.eachSeries(xorleft(askToggleSiteAnswers, sitesEnabled),enableSite,
         callback);
     },
     // disable sites
-    function(callback){
+    (callback)=>{
       async.eachSeries(xorleft(sitesEnabled, askToggleSiteAnswers),disableSite,
         callback);
     },
     // reload nginx configuration
-    function(callback){
+    (callback)=>{
       manageNginx('reload', callback);
     }
   ], (err)=>{
@@ -168,5 +232,34 @@ function toggleSites (sitesEnabled, askToggleSiteAnswers, toggleDoneCB){
       return toggleDoneCB(err);
     }
     toggleDoneCB();
+  });
+}
+
+// Add and remove virtual host file
+function addSite (pathToConfig){
+  console.log('addSite', pathToConfig);
+}
+
+function removeSite (site, removeSiteDoneCB){
+  // if the file is currently enabled, disable it before removing it.
+  async.series([
+    (callback)=>{
+      if(fileExists(`${NGINX_PATH}${sitesEnabledStr}/${site}`)){
+        disableSite(site, callback);
+      }else{
+        return callback();
+      }
+    }, (callback)=>{
+      sudoRemove(`${NGINX_PATH}${sitesAvailableStr}/${site}`, (err)=>{
+        if(err) return callback(err);
+        console.log(`${site} removed`);
+        return callback();
+      });
+    }, (callback)=>{
+      manageNginx('reload', callback);
+    }
+  ], (err)=>{
+    if(err) return removeSiteDoneCB(err);
+    return removeSiteDoneCB();
   });
 }
